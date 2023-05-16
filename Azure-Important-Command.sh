61813e10161c4bca6590ca7ddffce4acf2257e34
nrv-96 <62826272+nrv-96@users.noreply.github.com>
2023-05-16 11:49:12 +0530

    Update Azure-Important-Command.sh

--- Azure-Important-Command.sh
+++ Azure-Important-Command.sh
@@ -4,3 +4,5 @@ az group create --name 1 --location eastus
 2)Delete All Resource Group At Once Using CMD:
 FOR /F %g IN ('az group list --query "[].name" --output tsv') DO az group delete --name %g --yes
 
+3)Create KeyVault using Az Cli:
+az keyvault create --name "keyvault1605" --resource-group "1" --location "EastUS"
