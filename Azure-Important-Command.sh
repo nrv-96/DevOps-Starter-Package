fcbfdbdbfe4fabd6df1508234d41375d77b6de3e
nrv-96 <62826272+nrv-96@users.noreply.github.com>
2023-05-16 11:48:05 +0530

    Update Azure-Important-Command.sh

--- Azure-Important-Command.sh
+++ Azure-Important-Command.sh
@@ -1,3 +1,6 @@
-1)Delete All Resource Group At Once Using CMD:
+1)Create Resource Group:
+az group create --name 1 --location eastus
+
+2)Delete All Resource Group At Once Using CMD:
 FOR /F %g IN ('az group list --query "[].name" --output tsv') DO az group delete --name %g --yes
 
