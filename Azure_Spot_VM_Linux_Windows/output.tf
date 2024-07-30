2ccb6250ddbbfbdef514c456434e75ee172a5d1e
nrv-96 <62826272+nrv-96@users.noreply.github.com>
2024-07-30 11:46:02 +0530

    Update output.tf

--- Azure_Spot_VM_Linux_Windows/output.tf
+++ Azure_Spot_VM_Linux_Windows/output.tf
@@ -2,15 +2,15 @@ output "windows_pulic_ip" {
   value = azurerm_public_ip.window_vm_publicip.ip_address
 }
 
-output "linux_pulic_ip" {
-  value = azurerm_public_ip.linux_vm_publicip.ip_address
-}
+#output "linux_pulic_ip" {
+#  value = azurerm_public_ip.linux_vm_publicip.ip_address
+#}
 
 output "windows_vm_password" {
   value     = random_password.windows_vm.result
 }
 
-output "linux_vm_password" {
-  value     = random_password.linux_vm.result
-  sensitive = true
-}
+#output "linux_vm_password" {
+#  value     = random_password.linux_vm.result
+#  sensitive = true
+#}
