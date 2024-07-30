c26d70ab1abd5f19371d8a91c3abb2078dc83683
nrv-96 <62826272+nrv-96@users.noreply.github.com>
2024-07-30 11:50:24 +0530

    Update providers.tf

--- Azure_Spot_VM_Linux_Windows/providers.tf
+++ Azure_Spot_VM_Linux_Windows/providers.tf
@@ -1,13 +1,33 @@
 terraform {
-  required_version = ">=1.4.6"
+  required_version = ">= 1.7.4"
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "3.54.0"
+      version = ">= 3.94.0" #https://registry.terraform.io/providers/hashicorp/azurerm/latest
+    }
+    helm = {
+      source  = "hashicorp/helm"
+      version = ">= 2.12.1" #https://registry.terraform.io/providers/hashicorp/helm/latest
+    }
+    kubernetes = {
+      source  = "hashicorp/kubernetes"
+      version = ">= 2.26.0" #https://registry.terraform.io/providers/hashicorp/kubernetes/latest/docs
+    }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = "1.14.0" //https://registry.terraform.io/providers/gavinbunney/kubectl/latest/docs
+    }
+    local = {
+      source = "hashicorp/local"
+      version = "2.4.1" #https://registry.terraform.io/providers/hashicorp/local/latest/docs 
     }
   }
 }
 
 provider "azurerm" {
   features {}
+  subscription_id = var.subscription_id
+  tenant_id       = var.tenant_id
+  client_id       = var.client_id
+  client_secret   = var.client_secret
 }
