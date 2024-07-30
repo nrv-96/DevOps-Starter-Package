cf7b7d4a0aa208b4a509ec8ff9fd990092a9ea83
nrv-96 <62826272+nrv-96@users.noreply.github.com>
2024-07-30 11:49:42 +0530

    Update output.tf

--- Azure_Spot_VM_Linux_Windows/output.tf
+++ Azure_Spot_VM_Linux_Windows/output.tf
@@ -15,3 +15,21 @@ output "windows_vm_password" {
 #  value     = random_password.linux_vm.result
 #  sensitive = true
 #}
+
+variable "subscription_id" {
+  default = "0a457f9a-0b37-4bc0-b7b3-2f712bdb361a"
+  type = string
+}
+
+variable "tenant_id" {
+  default = "7834772d-924a-4eb1-b258-8d3c77a6cc23"
+  type = string
+}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
